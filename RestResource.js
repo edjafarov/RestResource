5f303d65120798e820d7c78e9ade23b36c6c2324
Eldar Djafarov <djkojb@gmail.com>
2015-02-20 19:27:14 +0200

    first commit

---
+++ RestResource.js
@@ -0,0 +1,15 @@
+module.exports = function RestResource(agent, PromisePipe){
+  
+  return {
+  	reg: function(name, baseUrl){
+  		//PromisePipe.use()
+  	}
+  }
+}
+/*
+{id}->
+.readUser()
+.forUser.readItem()
+
+.forUsers() -> PromisePipe
+*/
